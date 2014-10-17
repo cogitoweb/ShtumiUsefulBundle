4de7202b3686b8ee84affe42ace872f3cf311648
2zx <paolo.cazzitti@cogitoweb.it>
2014-10-17 11:32:14 +0200

    filtri multipli

--- Controller/AjaxAutocompleteJSONController.php
+++ Controller/AjaxAutocompleteJSONController.php
@@ -49,24 +49,57 @@ class AjaxAutocompleteJSONController extends Controller
 
         $property = $entity_inf['property'];
 
+        //inizio 2z ->
         if ($entity_inf['case_insensitive']) {
-                $where_clause_lhs = 'WHERE LOWER(e.' . $property . ')';
+                $where_clause_lhs = ' LOWER(e.' . $property . ')';
                 $where_clause_rhs = 'LIKE LOWER(:like)';
         } else {
 
-                $where_clause_lhs = 'WHERE e.' . $property;
+                $where_clause_lhs = ' e.' . $property;
                 $where_clause_rhs = 'LIKE :like';
         }
+        
+        $fixed_where = isset($entity_inf["fixed_where"]) ? str_replace('$class$', 'e', $entity_inf["fixed_where"]).' AND ' : '';
+        $join = isset($entity_inf["join"]) ? str_replace('$class$', 'e', $entity_inf["join"]) : '';
+        
+        $sql = 'SELECT distinct e.' . $property . ' %addselect%
+             FROM ' . $entity_inf['class'] . ' e '. $join . ' WHERE '.$fixed_where.' (' .
+             $where_clause_lhs . ' ' . $where_clause_rhs . ' ' . ' %addwhere%) ORDER BY ';
+        
+        $order_by = ' e.' . $property;
+        
+        $add_where = "";
+        $add_select = "";
+        foreach ($entity_inf["sub_properties"] as $sub) {
 
-        $results = $em->createQuery(
-            'SELECT distinct e.' . $property . '
-             FROM ' . $entity_inf['class'] . ' e ' .
-             $where_clause_lhs . ' ' . $where_clause_rhs . ' ' .
-            'ORDER BY e.' . $property)
-            ->setParameter('like', str_replace('\\', '\\\\',$like))
+            $add_select .= str_replace('$property$', $sub["property"], $sub["select_query_part"]);
+            $add_where .= str_replace('$property$', $sub["property"], $sub["where_query_part"]);
+            $order_by = str_replace('$default_sort$', $order_by, $sub["order_query_part"]);
+        }
+        
+        $sql = str_replace("%addselect%", $add_select, $sql);
+        $sql = str_replace("%addwhere%", $add_where, $sql);
+        $sql .= $order_by;
+
+        $query = $em->createQuery($sql)
+            ->setParameter('like', str_replace('\\', '\\\\',$like));
+        
+        // inject eventuale search plain
+        if(strpos($sql, ':search') !== false)
+        {
+            $query->setParameter('search', str_replace('\\', '\\\\',$letters));
+        }
+        // inject eventuale locale
+        if(strpos($sql, ':locale') !== false)
+        {
+            $query->setParameter('locale', $this->get('request')->get('_locale'));
+        }
+        
+        $results = $query
             ->setMaxResults($maxRows)
             ->getScalarResult();
-
+        // fine 2z ->
+        
         $res = array();
         foreach ($results AS $r){
             $res[] = $r[$entity_inf['property']];
